9e218b82b10e52daf5f762757c552f7a274f6257
omarelsheekh <31706237+omarelsheekh@users.noreply.github.com>
2019-07-03 11:31:10 +0200

    Update INI.py

--- INI.py
+++ INI.py
@@ -5,27 +5,20 @@ class KeyVal:
 
 class INI:
     def __init__(self,s):
-        self.sectionsName=[]
-        self.sectionsMaps=[[]]
-        i=0
+        self.dict={}
+        secName=""
         for line in s.split("\n"):
             if self.is_comment(line) or self.is_empty(line):
                 continue
             elif self.is_section(line):
                 secName=line[1:-1]
                 self.add_section(secName)
-                i=self.sectionsName.index(secName)
             elif self.is_keyval(line):
-                kv=KeyVal(line.split('=')[0],line.split('=')[1])
-                self.sectionsMaps[i].append(kv)
+                keyvalDict=self.dict[secName]
+                keyvalDict[line.split('=')[0].strip()]=line.split('=')[1].strip()
+                self.dict[secName]=keyvalDict
             else:
                 raise Exception("Can't process line {}".format(line))
-        # j=0
-        # for s in self.sectionsName:
-        #     print(print(s))
-        #     for m in self.sectionsMaps[j]:
-        #         print(m.key + m.val )
-        #     j+=1
 
     def is_keyval(self, line):
         pos = line.find('=');
@@ -40,32 +33,23 @@ class INI:
     def is_empty(self, line):
         return len(line) == 0
     def add_section(self,name):
-        self.sectionsName.append(name)
-        self.sectionsMaps.append([])
+        self.dict[name]={}
     def get_properity(self,secName,k):
-        i=self.sectionsName.index(secName)
-        mapArr=self.sectionsMaps[i]
-        for mm in mapArr:
-            if mm.key == k:
-                return mm.val
-        return None
+        keyvalDict=self.dict[secName]
+        return keyvalDict[k]
     def has_section(self,secName):
-        return  secName in self.sectionsName
+        return  secName in self.dict
     def has_properity(self,secName,k):
-        i = self.sectionsName.index(secName)
-        mapArr = self.sectionsMaps[i]
-        for mm in mapArr:
-            if mm.key == k:
-                return True
-        return False
+        keyvalDict=self.dict[secName]
+        return k in keyvalDict
 
 sample1 = """
 [general]
-appname=configparser
+appname = configparser
 version=0.1
 [author]
-name=xmonader
-email=notxmonader@gmail.com
+name =xmonader
+email =notxmonader@gmail.com
 """
 ini=INI(sample1)
 print(ini.get_properity("general","appname"))
